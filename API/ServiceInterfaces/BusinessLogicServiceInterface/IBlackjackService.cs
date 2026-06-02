276de99824e459772568a268fc9642687387e60c
CameronCS <camcstocks@gmail.com>
2026-06-03 01:06:41 +0200

    fixed bug

--- API/ServiceInterfaces/BusinessLogicServiceInterface/IBlackjackService.cs
+++ API/ServiceInterfaces/BusinessLogicServiceInterface/IBlackjackService.cs
@@ -7,4 +7,5 @@ public interface IBlackjackService
     Task<BlackjackState> DealAsync(Guid playerId, int bet, CancellationToken ct = default);
     Task<BlackjackState> HitAsync(Guid playerId, CancellationToken ct = default);
     Task<BlackjackResult> StandAsync(Guid playerId, CancellationToken ct = default);
+    Task<BlackjackResult> DoubleAsync(Guid playerId, CancellationToken ct = default);
 }
